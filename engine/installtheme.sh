782bd668b650766f79d25c04e480a12074bcacc4
Aurel Jared Dantis <jareddantis@gmail.com>
2015-05-11 13:39:37 +0800

    Unify everything into one script
    * Cleanup now merged into installtheme.sh
    * All phases (backup, theme, zipalign, replace) now unified into one
    single loop for each application type
    * Add stub for new way of restoring APKs by flashable ZIP

--- engine/installtheme.sh
+++ engine/installtheme.sh
@@ -8,6 +8,35 @@
 # Edify-like methods defined below are
 # copyright Chainfire 2011.
 
+# Declare busybox and output file descriptor
+bb="/tmp/busybox"
+OUTFD=$(ps | grep -v "grep" | grep -o -E "update_binary(.*)" | cut -d " " -f 3);
+
+[ $OUTFD != "" ] || OUTFD=$(ps | grep -v "grep" | grep -o -E "updater(.*)" | cut -d " " -f 3);
+
+# Check if ROM is Lollipop+ or not
+lpfiletree=$(ls /system/priv-app | grep -i '.apk')
+if [ $lpfiletree == "" ]; then
+	lollipop="1"
+	ui_print "- Adjusting engine for new app hierarchy"
+	checkdex() {
+		tmpvar=$(echo $2 | $bb sed -e 's/\.apk//g')
+		if [ -e ./classes.dex ]; then
+			rm -f /data/dalvik-cache/arm/system@$1@$tmpvar@$2.apk@classes.dex
+		fi
+	}
+else
+	checkdex() {
+		if [ -e ./classes.dex ]; then
+			if [ -e "/data/dalvik-cache/system@$1@$tmpvar@$2.apk@classes.dex" ]; then
+				rm -f /data/dalvik-cache/system@$1@$tmpvar@$2.apk@classes.dex
+			else
+				rm -f /cache/dalvik-cache/system@$1@$tmpvar@$2.apk@classes.
+			fi
+		fi
+	}
+fi
+
 # Define some methods
 ui_print() {
 	if [ $OUTFD != "" ]; then
@@ -17,292 +46,152 @@ ui_print() {
 		echo "${1}";
 	fi;
 }
-
 dir() {
 	$bb mkdir -p $1
 }
-
 zpln() {
+	if [ "$lollipop" -eq "1" ]; then
+		appDir=$(echo $1 | sed "s/\/$1\.apk")
+		$bb mkdir -p ./aligned/$appDir
+	fi
 	/tmp/zipalign -f -v 4 $1 ./aligned/$1
 }
-
 theme() {
 	/tmp/zip -r -q $1 $2
 }
 
-checkdex() {
-	if [ -e ./classes.dex ]; then
-		echo "# This is a flag for cleanup.sh" > $vrroot/flags/$1
-		echo "# to delete this app's dex entry" >> $vrroot/flags/$1
-	fi
-}
-
-# Declare busybox and output file descriptor
-bb="/tmp/busybox"
-OUTFD=$(ps | grep -v "grep" | grep -o -E "update_binary(.*)" | cut -d " " -f 3);
-
-[ $OUTFD != "" ] || OUTFD=$(ps | grep -v "grep" | grep -o -E "updater(.*)" | cut -d " " -f 3);
-
-# Check if ROM is Lollipop+ or not
-apiLevel=$(getprop ro.build.version.sdk)
-if [ $apiLevel -ge "21" ]; then
-	lollipop=1
-	ui_print "- Adjusting engine for Lollipop ROM"
-	appName() {
-		echo "$(echo $1 | $bb sed -e 's/\.apk//g')/$1"
-	}
-	appFolder() {
-		echo "$(echo $1 | $bb sed -e 's/\.apk//g')"
-	}
-else
-	appName() {
-		echo "$1"
-	}
-fi
-
 # Declare location shortcuts
-vrroot=/sdcard/vrtheme
-f1=$vrroot/system/app
-f1_1=$vrroot/system/priv-app
-f2=$vrroot/preload/system/app
-f3=$vrroot/system/framework
-f4=$vrroot/data/sec_app
-f5=$vrroot/apply/system/app
-f5priv=$vrroot/apply/system/priv-app
-f6=$vrroot/apply/preload/system/app
-f7=$vrroot/apply/system/framework
-f8=$vrroot/apply/data/sec_app
+vrroot="/data/tmp/eviltheme"
+vrbackup="/data/tmp/evt-backup"
+sysdir="/system/app"
+privdir="/system/priv-app"
+fwdir="/system/framework"
 
-# cleanup.sh will look here to see what apps need their dex entries refreshed.
-dir $vrroot/flags
-
-# Back up original APKs
-ui_print "- Backing up apps"
-[ -e $vrroot/system/app/* ] && sysapps=1 || sysapps=0
-if [ $sysapps -eq "1" ]; then
-	dir $vrroot/backup/system/app
-	dir $vrroot/apply/system/app
-	for f in $f1/*.apk; do
-		[ -e $f ] || break
-		ui_print " - $f"
-		cp /system/app/"$(appName $f)" $vrroot/apply/system/app/
-		cp /system/app/"$(appName $f)" $vrroot/backup/system/app/
-	done
-	ui_print ""
-fi
-
-[ -e $vrroot/system/priv-app/* ] && privapps=1 || privapps=0
-if [ $privapps -eq "1" ]; then
-	dir $vrroot/backup/system/priv-app
-	dir $vrroot/apply/system/priv-app
-	for f in $f1_1/*.apk; do
-		[ -e $f ] || break
-		ui_print " - $f"
-		cp /system/priv-app/"$(appName $f)" $vrroot/apply/system/priv-app/
-		cp /system/priv-app/"$(appName $f)" $vrroot/backup/system/priv-app/
-	done
-	ui_print ""
-fi
-
-[ -e $vrroot/preload/symlink/system/app/* ] && preload=1 || preload=0
-if [ $preload -eq "1" ]; then
-	ui_print "- Backing up preload apps"
-	dir $vrroot/backup/preload/symlink/system/app
-	dir $vrroot/apply/preload/symlink/system/app
-	for f in $f2/*.apk; do
-		[ -e $f ] || break
-		ui_print " - $f"
-		cp /preload/symlink/system/app/"$(appName $f)" $vrroot/apply/preload/symlink/system/app/
-		cp /preload/symlink/system/app/"$(appName $f)" $vrroot/backup/preload/symlink/system/app/
-	done
-	ui_print "Backups done for preload apps"
-	ui_print ""
-fi
-
-[ -e $vrroot/system/framework/* ] && framework=1 || framework=0
-if [ $framework -eq "1" ]; then
-	dir $vrroot/backup/system/framework
-	dir $vrroot/apply/system/framework
-	for f in $f3/*.apk; do
-		[ -e $f ] || break
-		ui_print " - $f"
-		cp /system/framework/$f $vrroot/apply/system/framework/
-		cp /system/framework/$f $vrroot/backup/system/framework/
-	done
-fi
-
-[ -e $vrroot/data/sec_data/* ] && datasecapps=1 || datasecapps=0
-if [ $datasecapps -eq "1" ]; then
-	dir $vrroot/backup/data/sec_data/
-	dir $vrroot/apply/data/sec_data/
-	for f in $f4/*.apk; do
-		[ -e $f ] || break
-		ui_print " - $f"
-		cp /data/sec_data/$f $vrroot/apply/data/sec_data/
-		cp /data/sec_data/$f $vrroot/backup/data/sec_data/
-	done
-fi
+# Make directories
+dir $vrbackup
+dir $vrroot/apply
+for d in $(ls $vrroot/apply/system/); do
+	mkdir -p "$vrbackup/$d"
+done
 
 # Theme the apps
 ui_print "- Themeing apps"
 
-if [ $sysapps -eq "1" ]; then
+if [ "$sysapps" -eq "1" ]; then
 	cd $vrroot/system/app/
-	for f in $f5/*.apk; do
-		[ -e $f ] || break
-		ui_print "* $f"
-		mv $vrroot/apply/system/app/$f $vrroot/apply/system/app/$f.zip
-		theme $vrroot/apply/system/app/$f.zip *
-		mv $vrroot/apply/system/app/$f.zip $vrroot/apply/system/app/$f
-		checkdex $f
+	dir aligned
+
+	for f in $(ls); do
+		# Backup APK
+		if [ "$lollipop" -eq "1" ]; then
+			dir $vrbackup/system/app/$f
+			dir $vrroot/apply/system/app/$f
+			cp /system/app/$f/$f.apk $vrbackup/system/app/$f/
+			cp /system/app/$f/$f.apk $vrroot/apply/system/app/$f/
+			appPath="$f/$f.apk"
+		else
+			cp /system/app/$f $vrbackup/system/app/
+			cp /system/app/$f $vrroot/apply/system/app/
+			appPath="$f"
+		fi
+
+		ui_print "* $appPath"
+
+		# Theme APK
+		mv $vrroot/apply/system/app/$appPath $vrroot/apply/system/app/$appPath.zip
+		theme $vrroot/apply/system/app/$appPath.zip *
+		mv $vrroot/apply/system/app/$appPath.zip $vrroot/apply/system/app/$appPath
+
+		# Refresh bytecode if necessary
+		checkdex "app" "$f"
+
+		# Zipalign APK
+		zpln $appPath
+
+		# Replace old APK
+		cp aligned/$appPath /system/app/
 	done
 fi
-
-if [ $privapps -eq "1" ]; then
+if [ "$privapps" -eq "1" ]; then
 	cd $vrroot/system/priv-app/
-	for f in $f5priv/*.apk; do
-		[ -e $f ] || break
-		ui_print "* $f"
-		mv $vrroot/apply/system/priv-app/$f $vrroot/apply/system/priv-app/$f.zip
-		theme $vrroot/apply/system/priv-app/$f.zip *
-		mv $vrroot/apply/system/priv-app/$f.zip $vrroot/apply/system/priv-app/$f
-		checkdex $f
+	dir aligned
+
+	for f in $(ls); do
+		# Backup APK
+		if [ "$lollipop" -eq "1" ]; then
+			dir $vrbackup/system/priv-app/$f
+			dir $vrroot/apply/system/priv-app/$f
+			cp /system/priv-app/$f/$f.apk $vrbackup/system/priv-app/$f/
+			cp /system/priv-app/$f/$f.apk $vrroot/apply/system/priv-app/$f/
+			appPath="$f/$f.apk"
+		else
+			cp /system/priv-app/$f $vrbackup/system/priv-app/
+			cp /system/priv-app/$f $vrroot/apply/system/priv-app/
+			appPath="$f"
+		fi
+
+		ui_print "* $appPath"
+
+		# Theme APK
+		mv $vrroot/apply/system/priv-app/$appPath $vrroot/apply/system/priv-app/$appPath.zip
+		theme $vrroot/apply/system/priv-app/$appPath.zip *
+		mv $vrroot/apply/system/priv-app/$appPath.zip $vrroot/apply/system/priv-app/$appPath
+
+		# Refresh bytecode if necessary
+		checkdex "priv-app" "$f"
+
+		# Zipalign APK
+		zpln $appPath
+
+		# Replace old APK
+		cp aligned/$appPath /system/priv-app/
 	done
 fi
 
-if [ $preload -eq "1" ]; then
-	cd $vrroot/preload/symlink/system/app/
-	for f in $f6/*.apk; do
-		[ -e $f ] || break
-		ui_print " - $f"
-		mv $vrroot/apply/preload/symlink/system/app/$f $vrroot/apply/preload/symlink/system/app/$f.zip
-		theme $vrroot/apply/preload/symlink/system/app/$f.zip *
-		mv $vrroot/apply/preload/symlink/system/app/$f.zip $vrroot/apply/preload/symlink/system/app/$f
-		checkdex $f
-	done
-fi
-
-if [ $framework -eq "1" ]; then
+if [ "$framework" -eq "1" ]; then
 	cd $vrroot/system/framework/
-	for f in $f7/*.apk; do
-		[ -e $f ] || break
-		ui_print " - $f"
-		mv $vrroot/apply/system/framework/$f $vrroot/apply/system/framework/$f.zip
-		theme $vrroot/apply/system/framework/$f.zip *
-		mv $vrroot/apply/system/framework/$f.zip $vrroot/apply/system/framework/$f
-		checkdex $f
+	dir aligned
+
+	for f in $(ls); do
+		# Backup APK
+		if [ "$lollipop" -eq "1" ]; then
+			dir $vrbackup/system/framework/$f
+			dir $vrroot/apply/system/framework/$f
+			cp /system/framework/$f/$f.apk $vrbackup/system/framework/$f/
+			cp /system/framework/$f/$f.apk $vrroot/apply/system/framework/$f/
+			appPath="$f/$f.apk"
+		else
+			cp /system/framework/$f $vrbackup/system/framework/
+			cp /system/framework/$f $vrroot/apply/system/framework/
+			appPath="$f"
+		fi
+
+		ui_print "* $appPath"
+
+		# Theme APK
+		mv $vrroot/apply/system/framework/$appPath $vrroot/apply/system/framework/$appPath.zip
+		theme $vrroot/apply/system/framework/$appPath.zip *
+		mv $vrroot/apply/system/framework/$appPath.zip $vrroot/apply/system/framework/$appPath
+
+		# Refresh bytecode if necessary
+		checkdex "framework" "$f"
+
+		# Zipalign APK
+		zpln $appPath
+
+		# Replace old APK
+		cp aligned/$appPath /system/framework/
 	done
 fi
 
-if [ $datasecapps -eq "1" ]; then
-	cd $vrroot/data/sec_data/
-	for f in $f8/*.apk; do
-		[ -e $f ] || break
-		ui_print " - $f"
-		mv $vrroot/apply/data/sec_data/$f $vrroot/apply/data/sec_data/$f.zip
-		theme $vrroot/apply/data/sec_data/$f.zip *
-		mv $vrroot/apply/data/sec_data/$f.zip $vrroot/apply/data/sec_data/$f
-		checkdex $f
-	done
-fi
+# Create flashable restore zip
+datetime=$($bb date +"%m%d%y-%H%M%S")
+cd $vrbackup
+dir /data/eviltheme-backup
+mv /data/tmp/eviltheme/vrtheme_restore.zip /data/tmp/eviltheme/restore-$datetime.zip
+theme /data/tmp/eviltheme/restore-$datetime.zip *
+mv /data/tmp/eviltheme/restore-$datetime.zip /data/eviltheme-backup
 
-# Zipalign all the APKs
-ui_print "- Zipaligning themed apps"
-if [ $sysapps -eq "1" ]; then
-	cd $vrroot/apply/system/app/
-	$bb mkdir aligned
-	for f in $f5/*.apk; do
-		[ -e $f ] || break
-		zpln $f
-	done
-fi
-
-if [ $privapps -eq "1" ]; then
-	cd $vrroot/apply/system/priv-app/
-	$bb mkdir aligned
-	for f in $f5priv/*.apk; do
-		[ -e $f ] || break
-		zpln $f
-	done
-fi
-
-if [ $preload -eq "1" ]; then
-	cd $vrroot/apply/preload/symlink/system/app/
-	$bb mkdir aligned
-	for f in $f6/*.apk; do
-		[ -e $f ] || break
-		zpln $f
-	done
-fi
-
-if [ $framework -eq "1" ]; then
-	cd $vrroot/apply/system/framework/
-	$bb mkdir aligned
-	for f in $f7/*.apk; do
-		[ -e $f ] || break
-		zpln $f
-	done
-fi
-
-if [ $datasecapps -eq "1" ]; then
-	cd $vrroot/apply/data/sec_data/
-	$bb mkdir aligned
-	for f in $f8/*.apk; do
-		[ -e $f ] || break
-		zpln $f
-	done
-fi
-
-# Move each new app back to its original location
-if [ $sysapps -eq "1" ]; then
-	cd $vrroot/apply/system/app/aligned/
-	if [ $lollipop -eq "1" ]; then
-		for f in $f5/aligned/*.apk; do
-			[ -e $f ] || break
-			cp $f /system/app/"$(appFolder $f)"/
-		done
-	else
-		cp * /system/app/
-	fi
-	chmod -R 644 /system/app/*
-fi
-
-if [ $privapps -eq "1" ]; then
-	cd $vrroot/apply/system/priv-app/aligned/
-	if [ $lollipop -eq "1" ]; then
-		for f in $f5-priv/aligned/*.apk; do
-			[ -e $f ] || break
-			cp $f /system/priv-app/"$(appFolder $f)"/
-		done
-	else
-		cp * /system/priv-app/
-	fi
-	chmod -R 644 /system/priv-app/*
-fi
-
-if [ $preload -eq "1" ]; then
-	cd $vrroot/apply/preload/symlink/system/app/aligned/
-	if [ $lollipop -eq "1" ]; then
-		for f in $f6/aligned/*.apk; do
-			[ -e $f ] || break
-			cp $f /preload/symlink/system/app/"$(appFolder $f)"/
-		done
-	else
-		cp * /preload/symlink/system/app/
-	fi
-	chmod -R 644 /preload/symlink/system/app/*
-fi
-
-if [ $framework -eq "1" ]; then
-	cd $vrroot/apply/system/framework/aligned/
-	cp * /system/framework
-	chmod -R 644 /system/framework/*
-fi
-
-if [ $datasecapps -eq "1" ]; then
-	cd $vrroot/apply/data/sec_data/aligned/
-	cp * /data/sec_data/
-	chmod -R 644 /data/sec_data/*
-fi
+# Cleanup
 
 exit 0
