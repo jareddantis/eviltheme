a6d90c88afa04e395c207801a811268f2490ce6e
Jared Dantis <jareddantis@gmail.com>
2017-07-19 16:49:53 +0800

    scripts: Warn on pre-13.1 Magisk versions

--- META-INF/tk/aureljared/eviltheme/magisk-setup.sh
+++ META-INF/tk/aureljared/eviltheme/magisk-setup.sh
@@ -4,6 +4,7 @@
 # https://github.com/aureljared/eviltheme
 . "$1"
 target="$2"
+templateVersion="$3"
 
 # Create module.prop
 cat << EOF > "$target/module.prop"
@@ -15,7 +16,7 @@ author=$themeAuthor
 description=$themeDescription
 donate=$themeDonate
 support=$themeSupport
-template=4
+template=$templateVersion
 EOF
 touch "$target/auto_mount"
 exit 0
